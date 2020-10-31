231c9800fbd0d6054d70adf11aa12511b522fe96
landaudiogo <diogo.hewitt.landau@hotmail.com>
2020-10-31 22:21:21 +0000

    writer first commit

--- projeto_1/writer.c
+++ projeto_1/writer.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-#include <stdin.h>
+#include <termios.h>
+#include <string.h>
+#include <inttypes.h>
+
+int main(int argc, char **argv) {
+    int fd;
+
+    if(argc<1 || strncmp(argv[1], "/dev/pts/", 9) != 0) 
+    {
+        printf("Expected first argument must be of type: \"/dev/pts/*\"\n");
+    }
+
+    return 0;
+}
