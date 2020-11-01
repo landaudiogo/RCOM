38550eea60801cd3c3d34c77857b4f92d020eac9
Goncalo F <up201707382@fe.up.pt>
2020-11-01 01:55:46 +0000

    autosave

--- projeto_1/reader.c
+++ projeto_1/reader.c
@@ -12,94 +12,125 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+volatile int STOP = FALSE;
 
-void signal_handler_IO (int status);   /* definition of signal handler */
-int wait_flag=TRUE;                    /* TRUE while no signal received */
+void signal_handler_IO(int status); /* definition of signal handler */
+int wait_flag = TRUE;               /* TRUE while no signal received */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    struct sigaction saio;           /* definition of signal action */
-    char buf[255];
-    int i, num;
+  int fd;
+  char buf[1];
+  int i, nread;
 
-    //Uses either COM1 or COM2     
-  	if ((argv[1] > 65) && ( (argv[1] != 'a') || (argv[1] != 'A') || (argv[1] != 'b') || (argv[1] != 'B') ) ) {
-      printf("a para ttyS0, b para ttyS1, n para /dev/pts/n");
-      exit(1);
-    }
+  //Uses either COM1 or COM2
+  if ((argv[1] > 65) && ((argv[1] != 'a') || (argv[1] != 'A') || (argv[1] != 'b') || (argv[1] != 'B')))
+  {
+    printf("a para ttyS0, b para ttyS1, n para /dev/pts/n");
+    exit(1);
+  }
 
-    /* open the device to be non-blocking (read will return immediatly) */
-    fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
-//    fd = open(argv[1], O_RDWR | O_NOCTTY);
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  /* open the device to be non-blocking (read will return immediatly) */
+  fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
+  //    fd = open(argv[1], O_RDWR | O_NOCTTY);
+  if (fd < 0)
+  {
+    perror(argv[1]);
+    exit(-1);
+  }
 
-
-    /* To receive the SIGIO we need to perform three steps: */
-    /* 1. establish a signal handler for the signal - sigaction() */
-    /* 2. set the pid or gid to receive the signal for the descriptor */
-    /* 3. enable asynchronous I/O on the descriptor*/
-
-    /* install the signal handler before making the device asynchronous */
-    saio.sa_handler = signal_handler_IO;/*signal handler address or SI_IGN ou SIG_DFL*/
-    saio.sa_flags = 0;          /*gives  a  mask of signals which should */
-                                /*be blocked during execution of the */
-                                /*signal handler.  In addition,  the signal*/
-                                /*which triggered the handler will be blocked,*/
-                                /*unless the SA_NODEFER or SA_NOMASK flags */
-                                /*are used.*/
-    saio.sa_restorer = NULL;    /*obsolete*/
-    sigaction(SIGIO,&saio,NULL);
-
-    /* allow the process to receive SIGIO */
-    /* set the process ID or process group ID to receive */
-    /* the SIGIO signal. A positive arg specifies the pid. */
-    /* A negative arg implies a process group ID equal to */
-    /* absolute value of arg*/
-    fcntl(fd, F_SETOWN, getpid());
-    /* Make the file descriptor asynchronous (the manual page says only
-       O_APPEND and O_NONBLOCK, will work with F_SETFL...) */
-    fcntl(fd, F_SETFL, FASYNC);
-
-    tcgetattr(fd,&oldtio); /* save current port settings */
-    /* set new port settings for canonical input processing */
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR | ICRNL;
-    newtio.c_oflag = 0;
-    newtio.c_lflag = ICANON;
-    newtio.c_cc[VMIN]=1;
-    newtio.c_cc[VTIME]=0;
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd,TCSANOW,&newtio);
-
-    /* loop while waiting for input. normally we would do something
-       useful here */
-    while (STOP==FALSE) {
-      write(1, ".", 1);usleep(100000);
-      /* after receiving SIGIO, wait_flag = FALSE, input is available
-         and can be read */
-      if (wait_flag==FALSE) {
-        res = read(fd,buf,255);
-        buf[res]=0;
-        for (i = 0; i < res; i++) printf("%c", buf[i]);
-        if (res==1) STOP=TRUE; /* stop loop if only a CR was input */
-        wait_flag = TRUE;      /* wait for new input */
-      }
-    }
-    /* restore old port settings */
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+  nread = LLREAD(fd, buf);
 }
 
-/***************************************************************************
-* signal handler. sets wait_flag to FALSE, to indicate above loop that     *
-* characters have been received.                                           *
-***************************************************************************/
-
-void signal_handler_IO (int status)
+int LLREAD(int fd, char *buffer) //retorna n de chars lidos
 {
-    printf("received SIGIO signal");
-    wait_flag = FALSE;
+  int state;
+  int flag1_rcv = 0;
+  int a_rcv = 0;
+  int c_rcv = 0;
+  int bcc1_rcv = 0;
+  int bcc2_rcv = 0;
+  int flag2_rcv = 0;
+
+  if (STOP == FALSE) //nao sei o q isto e
+  {
+    state = 0;
+
+    switch (state) //FSM para SET apenas (WIP)
+    {
+
+    case 0: //nada, start
+    {
+      flag1_rcv = isFlag();
+      if (flag1_rcv)
+        state = 1;
+      break;
+    }
+
+    case 1:
+    {
+      a_rcv = isAddress(); //1 se address, 2 se flag, -1 outro
+      if (a_rcv == 1)
+        state = 2;
+      else if (a_rcv == 2)
+        state = 1;
+      else
+        state = 0;
+      break;
+    }
+    case 2:
+    {
+      c_rcv = isControl(); //1 se c, 2 se f, -1 outro
+      if (c_rcv == 1)
+        state = 3;
+      else if (c_rcv == 2)
+        state = 2;
+      else
+        state = 0;
+      break;
+    }
+    case 3:
+    {
+      bcc1_rcv = isBcc1(); //1 se bcc1, 2 se f, -1 outro
+      if (bcc1_rcv == 1)
+        state = 4;
+      else if (bcc1_rcv == 2)
+        state = 1;
+      else
+        state = 0;
+      break;
+    }
+    case 4:
+    {
+      flag2_rcv = isflag();
+      if (flag2_rcv == 1)
+        state = 5;
+      else if (flag2_rcv == -1)
+        state = 0;
+      break;
+    }
+    case 5:
+    {
+      STOP = TRUE; //nao sei o que e isto
+      wait_flag = TRUE; //nem isto
+      break;
+    }
+    }
+  }
+  /* while (STOP == FALSE)
+    {
+      write(1, ".", 1);
+      usleep(100000);
+       after receiving SIGIO, wait_flag = FALSE, input is available
+         and can be read */
+  /*  if (wait_flag == FALSE)
+      {
+        int res = read(fd, buffer, 255);
+        buffer[res] = 0;
+        for (int i = 0; i < res; i++)
+          printf("%c", buffer[i]);
+        if (res == 1)
+          STOP = TRUE;    /* stop loop if only a CR was input */
+  /*  wait_flag = TRUE; /* wait for new input */
+  //  }
 }
